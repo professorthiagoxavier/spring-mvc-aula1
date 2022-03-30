17472057323b87e99ac8226e75b5b779b2c7eb73
Thiago Xavier <thiago.xavier@unidas.com.br>
2022-03-30 20:59:48 -0300

    cadastro de produtos

--- src/main/java/br/com/fiap/appprodutoteste/produto/controllers/ProdutoController.java
+++ src/main/java/br/com/fiap/appprodutoteste/produto/controllers/ProdutoController.java
@@ -3,12 +3,11 @@ package br.com.fiap.appprodutoteste.produto.controllers;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import br.com.fiap.appprodutoteste.produto.model.Produto;
 import br.com.fiap.appprodutoteste.produto.repositories.ProdutoRepository;
 
@@ -27,12 +26,40 @@ public class ProdutoController {
 		Produto produto3 = new Produto(new Long(4), "chocolate branco", 30, new BigDecimal(20));
 		List<Produto> produtos = Arrays.asList(produto1, produto2, produto3);
 		
-		
 		List<Produto> produtoDaRepository = produtoRepository.findAll();
 		modelView.addObject("listarProdutos", produtoDaRepository);
 		
 		return modelView;
 	}
+	
+	@GetMapping("/produtos/criar")
+	public ModelAndView criar() {
+		ModelAndView modelView = new ModelAndView("produtos/criar");
+		return modelView;
+	}
+	
+	@PostMapping("produtos")
+	public String salvar(Produto produto ) {
+		
+		System.out.println(produto.getNome());
+		
+		produtoRepository.save(produto);
+		
+		return "redirect:/produtos";
+			
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 
 }
