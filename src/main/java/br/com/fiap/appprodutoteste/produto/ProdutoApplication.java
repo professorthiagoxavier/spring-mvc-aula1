a78a76129b1e9d818e20d6946e839fea3e860580
Thiago Xavier <thiago.xavier@unidas.com.br>
2022-03-30 22:10:08 -0300

    mapeamento de produto

--- src/main/java/br/com/fiap/appprodutoteste/produto/ProdutoApplication.java
+++ src/main/java/br/com/fiap/appprodutoteste/produto/ProdutoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"br.com.fiap.appprodutoteste.produto.controllers"})
+@ComponentScan({"br.com.fiap.appprodutoteste.produto.controllers, br.com.fiap.appprodutoteste.produto.config"})
 public class ProdutoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/fiap/appprodutoteste/produto/controllers/ProdutoController.java
+++ src/main/java/br/com/fiap/appprodutoteste/produto/controllers/ProdutoController.java
@@ -3,19 +3,26 @@ package br.com.fiap.appprodutoteste.produto.controllers;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import br.com.fiap.appprodutoteste.produto.dto.ProdutoDto;
 import br.com.fiap.appprodutoteste.produto.model.Produto;
 import br.com.fiap.appprodutoteste.produto.repositories.ProdutoRepository;
+
 
 @Controller
 public class ProdutoController {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
 	
 	@GetMapping("/produtos")
 	public ModelAndView index() {
@@ -39,11 +46,10 @@ public class ProdutoController {
 	}
 	
 	@PostMapping("produtos")
-	public String salvar(Produto produto ) {
+	public String salvar(ProdutoDto produto ) {
+		Produto produtoEntity = modelMapper.map(produto, Produto.class);
 		
-		System.out.println(produto.getNome());
-		
-		produtoRepository.save(produto);
+		produtoRepository.save(produtoEntity);
 		
 		return "redirect:/produtos";
 			
